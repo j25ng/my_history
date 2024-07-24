1abe537702b9557580cec6fd1aec8978de5ae13c
j25ng <jsan2861@gmail.com>
2024-07-24 19:15:59 +0900

    update v0.2.0

---
+++ src/my_history/arg_parse.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import argparse
+import sys
+
+def argp():
+    parser = argparse.ArgumentParser(description="history search programm")
+    group = parser.add_argument_group()
+    group.add_argument("-c", "--count", type=str, action="store", help="my-history -c <cmd>")
+    group.add_argument("-t", "--top", type=int, action="store", help="my-history -t <num>")
+    group.add_argument("-d", "--date", type=str, action="store", help="my-history -d <date>")
+
+    args = parser.parse_args()
+
+    if args.count:
+        cnt(args.count)
+    elif args.top and args.date:
+        top(args.top, args.date)
+    else:
+        parser.print_help()
+
+def cnt(q):
+    df = pd.read_parquet('~/data/parquet')
+    fdf = df[df['cmd'] == q ]
+    cnt = fdf['cnt'].sum()
+    print(f'{q} 사용 횟수는 {cnt}회 입니다.')
+
+def top(n, date):
+    df = pd.read_parquet('~/data/parquet')
+    fdf = df[df['dt'] == date].sort_values(by='cnt', ascending=False).head(n)
+    ddf = fdf.drop(columns=['dt']).to_string(index=False)
+    print(ddf)
+
