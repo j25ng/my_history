c1befc0caabfe8bfb184f260f3f2770b3c450226
j25ng <jsan2861@gmail.com>
2024-07-25 17:33:45 +0900

    tabulate

--- src/my_history/arg_parse.py
+++ src/my_history/arg_parse.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import argparse
 import sys
+from tabulate import tabulate
 
 def argp():
     parser = argparse.ArgumentParser(description="history search programm")
     group = parser.add_argument_group()
-    group.add_argument("-c", "--count", type=str, action="store", help="my-history -c <cmd>")
-    group.add_argument("-t", "--top", type=int, action="store", help="my-history -t <num>")
-    group.add_argument("-d", "--date", type=str, action="store", help="my-history -d <date>")
+    group.add_argument("-c", "--count", type=str, help="my-history -c <cmd>")
+    group.add_argument("-t", "--top", type=int, help="my-history -t <num>")
+    group.add_argument("-d", "--date", type=str, help="my-history -d <date>")
 
     args = parser.parse_args()
 
@@ -30,6 +31,6 @@ def cnt(q):
 def top(n, date):
     df = pd.read_parquet('~/data/parquet')
     fdf = df[df['dt'] == date].sort_values(by='cnt', ascending=False).head(n)
-    ddf = fdf.drop(columns=['dt']).to_string(index=False)
-    print(ddf)
+    ddf = fdf.drop(columns=['dt'])
+    print(tabulate(ddf))
 
